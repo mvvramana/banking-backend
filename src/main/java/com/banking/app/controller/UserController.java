90e3a0c1439a4d97bb33a579f0c6933754a76c2c
mvvramana <srinumedisetti35@gamil.com>
2026-03-14 11:43:32 +0530

    Implemented JWT token generation

--- src/main/java/com/banking/app/controller/UserController.java
+++ src/main/java/com/banking/app/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.banking.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.banking.app.entity.User;
+import com.banking.app.security.JwtUtil;
 import com.banking.app.service.UserService;
 
 import jakarta.validation.Valid;
@@ -15,10 +20,14 @@ public class UserController {
     @Autowired
     private UserService userService;
     
+    @Autowired
+    private JwtUtil jwtUtil;
+    
     @PostMapping("/login")
-    public User login(@RequestParam String email,
-                      @RequestParam String password) {
-        return userService.login(email, password);
+    public String login(@RequestParam String email,
+                        @RequestParam String password) {
+        userService.login(email, password);
+        return jwtUtil.generateToken(email);
     }
     
     @PostMapping("/register")
